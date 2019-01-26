544f9eb7f4f15cb5e8bafb10236dfc3baf15408e
pedrocaseiro <pcaseiro96@gmail.com>
2019-01-26 23:17:22 +0000

    Add a supervisor

---
+++ chapter8/persistable_todo_cache/lib/todo/system.ex
@@ -0,0 +1,14 @@
+defmodule Todo.System do
+  use Supervisor
+
+  def init(_) do
+    Supervisor.init(
+      [Todo.Cache],
+      strategy: :one_for_one
+    )
+  end
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, nil)
+  end
+end
