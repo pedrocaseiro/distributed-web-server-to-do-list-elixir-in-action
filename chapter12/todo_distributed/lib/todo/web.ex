dd52d4443633a66402559ac9ca4568533c510540
pedrocaseiro <pcaseiro96@gmail.com>
2019-01-30 22:49:04 +0000

    Chapter 12 - nodes

---
+++ chapter12/todo_distributed/lib/todo/web.ex
@@ -0,0 +1,55 @@
+defmodule Todo.Web do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  def child_spec(_arg) do
+    Plug.Adapters.Cowboy.child_spec(
+      scheme: :http,
+      options: [port: Application.fetch_env!(:todo, :port)],
+      plug: __MODULE__
+    )
+  end
+
+  # curl 'http://localhost:5454/entries?list=bob&date=2018-12-19'
+  get "/entries" do
+    conn = Plug.Conn.fetch_query_params(conn)
+    list_name = Map.fetch!(conn.params, "list")
+    date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
+
+    entries =
+      list_name
+      |> Todo.Cache.server_process()
+      |> Todo.Server.entries(date)
+
+    formatted_entries =
+      entries
+      |> Enum.map(&"#{&1.date} #{&1.title}")
+      |> Enum.join("\n")
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, formatted_entries)
+  end
+
+  # curl -d '' 'http://localhost:5454/add_entry?list=bob&date=2018-12-19&title=Dentist'
+  post "/add_entry" do
+    conn = Plug.Conn.fetch_query_params(conn)
+    list_name = Map.fetch!(conn.params, "list")
+    title = Map.fetch!(conn.params, "title")
+    date = Date.from_iso8601!(Map.fetch!(conn.params, "date"))
+
+    list_name
+    |> Todo.Cache.server_process()
+    |> Todo.Server.add_entry(%{title: title, date: date})
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/plain")
+    |> Plug.Conn.send_resp(200, "OK")
+  end
+
+  match _ do
+    Plug.Conn.send_resp(conn, 404, "not found")
+  end
+end
